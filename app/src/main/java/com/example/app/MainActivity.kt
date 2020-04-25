cbfb5db43e00922f6ecb10851b4430ef894aacef
JVGS1 <50575899+JVGS1@users.noreply.github.com>
2020-04-25 05:35:29 +0100

    FInished splash annimation and added in second page

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,17 +1,53 @@
 package com.example.app
 
+import android.content.Intent
+//import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //Variables for animation
+
+
+
+    private lateinit var image:ImageView
+
+    private lateinit var logo:TextView
+    private lateinit var appName:TextView
+
+    private lateinit var topAnim:Animation
+    private lateinit var bottomAnim:Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Hiding status bar
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         setContentView(R.layout.activity_main)
+
+
+        //Animation
+         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_splash_animation)
+         bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_splash_animation)
+
+        //hooks
+        appName = findViewById(R.id.SplashText)
+
+
+        appName.setAnimation(topAnim)
+
+        Handler().postDelayed({
+            val intent = Intent(this, reminders::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
+
--- app/src/main/java/com/example/app/reminders.kt
+++ app/src/main/java/com/example/app/reminders.kt
@@ -2,11 +2,13 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class reminders : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_reminders)
     }
 }
